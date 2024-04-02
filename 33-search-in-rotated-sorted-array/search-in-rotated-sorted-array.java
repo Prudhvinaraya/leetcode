b6a0ee6b727b07b9e1c45ee51d97ef947dfd4cd7
Prudhvi narayana BANDARU <93300939+Prudhvinaraya@users.noreply.github.com>
2024-04-02 21:48:55 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (64.96%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,11 +1,46 @@
 class Solution {
     public int search(int[] nums, int target) {
         //writing logic for O(n)
-        for(int i=0;i<nums.length;i++)
-        {
-            if(nums[i] ==target)
-            {
-                return i;
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     if(nums[i] ==target)
+        //     {
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
+        //for O(logn) logic we do have binary search algorithm 
+        //we will have left and right poinitng to ends of array
+        //calculate the mid
+        // Initialize left and right pointers as indices
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+            
+            // If the left half is sorted
+            if (nums[left] <= nums[mid]) {
+                // If the target is within the sorted left half
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            // If the right half is sorted
+            else {
+                // If the target is within the sorted right half
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
             }
         }
         return -1;
