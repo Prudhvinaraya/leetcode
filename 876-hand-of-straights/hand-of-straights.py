e0dc4e4ab5be91265b179d6f85796b4580b52588
Prudhvi narayana BANDARU <93300939+Prudhvinaraya@users.noreply.github.com>
2024-06-06 21:21:04 +0530

    Time: 156 ms (80.80%) | Memory: 18.1 MB (86.00%) - LeetSync

---
+++ 876-hand-of-straights/hand-of-straights.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isNStraightHand(self, hand, groupSize):
+        if len(hand) % groupSize != 0:
+            return False
+
+        # Frequency map using Counter
+        count = Counter(hand)
+
+        # Sorted keys
+        sorted_keys = sorted(count.keys())
+
+        for key in sorted_keys:
+            if count[key] > 0:
+                for i in range(1, groupSize):
+                    if count[key + i] < count[key]:
+                        return False
+                    count[key + i] -= count[key]
+        return True
+        
