abed006363e3a213e3cb719cc3b31ab9b76ec16a
Prudhvi narayana BANDARU <93300939+Prudhvinaraya@users.noreply.github.com>
2024-07-17 23:02:11 +0530

    Time: 40 ms (38.17%) | Memory: 17.7 MB (54.11%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -6,15 +6,26 @@
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         ##we will  consider curr,nxt,prev
-        curr=head
-        nxt=None
-        prev=None
-        while(curr):
-            nxt=curr.next
-            curr.next=prev ##poinintg to null
-            prev=curr ##pointing to first ele in iteration 1
-            curr=nxt
+        ##recursive
+        if head ==None or head.next==None:
+            return head
+        newhead=self.reverseList(head.next)
+        nxt=head.next
+        nxt.next=head
+        head.next=None
+        return newhead
         
-        return prev
+        
+        ##Iterative
+#         curr=head
+#         nxt=None
+#         prev=None
+#         while(curr):
+#             nxt=curr.next
+#             curr.next=prev ##poinintg to null
+#             prev=curr ##pointing to first ele in iteration 1
+#             curr=nxt
+        
+#         return prev
             
         
