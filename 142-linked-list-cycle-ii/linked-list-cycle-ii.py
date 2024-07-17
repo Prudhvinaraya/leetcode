4c4d520f2564f2a0ccc1b7b35fa22b41a0723287
Prudhvi narayana BANDARU <93300939+Prudhvinaraya@users.noreply.github.com>
2024-07-18 00:16:43 +0530

    Time: 48 ms (27.84%) | Memory: 19.1 MB (58.42%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.py
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.py
@@ -6,11 +6,29 @@
 
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        dict={}
-        curr_node=head
-        while(curr_node):
-            if curr_node in dict:
-                return curr_node
-            dict[curr_node]=True
-            curr_node=curr_node.next
+        
+        ##brute force
+#         dict={}
+#         curr_node=head
+#         while(curr_node):
+#             if curr_node in dict:
+#                 return curr_node
+#             dict[curr_node]=True
+#             curr_node=curr_node.next
+        ##optimal
+        #1)Detect the loop using tortoise and hare method
+        slow=head
+        fast=head
+        while(fast is not None and fast.next is not None):
+            slow=slow.next
+            fast=fast.next.next
+            if (slow==fast):
+                ##yes there is loop
+                #2)Task is to find the start of the loop
+                slow=head
+                while(slow!=fast):
+                    slow=slow.next
+                    fast=fast.next
+                return slow
+        return None
         
