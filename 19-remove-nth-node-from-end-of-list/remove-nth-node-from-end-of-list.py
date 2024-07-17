93e76a8316dfb6c7d341e1efa15e147b210a0cda
Prudhvi narayana BANDARU <93300939+Prudhvinaraya@users.noreply.github.com>
2024-07-17 13:38:43 +0530

    Time: 41 ms (24.91%) | Memory: 16.5 MB (64.34%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -1,25 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        # Step 1: Calculate the length of the linked list
-        curr_node = head
-        count = 0
-        while curr_node:
-            count += 1
-            curr_node = curr_node.next
-        
-        # Handle edge case: Remove head node
-        if n == count:
+        ##Bruteforce
+        curr_node=head
+        count=0
+        while(curr_node):
+            count+=1
+            curr_node=curr_node.next
+        ##count=5
+        if count==1:
+            return None
+        if count==n:
             return head.next
-        
-        # Calculate the position of the node to remove
-        index = count - n
-        
-        # Step 2: Traverse to the node just before the one to remove
-        curr_node = head
+        index=count-n ##3
+        curr_node=head
+        c=0
         for _ in range(index - 1):
             curr_node = curr_node.next
-        
-        # Step 3: Remove the nth node from the end
-        curr_node.next = curr_node.next.next
-        
+        curr_node.next=curr_node.next.next
         return head
+        
