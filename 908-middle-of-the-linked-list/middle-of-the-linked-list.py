02d520392bc224769e1d9ba3b4daf02435f84f68
Prudhvi narayana BANDARU <93300939+Prudhvinaraya@users.noreply.github.com>
2024-07-17 13:14:06 +0530

    Time: 37 ms (43.50%) | Memory: 16.5 MB (13.56%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.py
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -5,14 +5,28 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        slow = head
-        fast = head
+        ##Brute force 0(n+n/2)
+        # curr_node=head
+        # count=0
+        # # lst=[]
+        # while(curr_node != None):
+        #     curr_node=curr_node.next
+        #     count+=1
+        # ##i got count =5
+        # curr_node=head
+        # c=0
+        # while(curr_node!=None and c!=count//2):
+        #     c+=1
+        #     curr_node=curr_node.next
+        # head=curr_node
         
-        # Use fast.next is not None to check fast and fast.next together
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
         
+        ##optimal solution using tortoise and hare method
+        slow=head
+        fast=head
+        while(fast and fast.next):
+            slow=slow.next
+            fast=fast.next.next
         return slow
         
         
