e0166c41e64e2f2423c95ab1086cd0aca8c0f76a
Prudhvi narayana BANDARU <93300939+Prudhvinaraya@users.noreply.github.com>
2024-07-18 13:24:03 +0530

    Time: 41 ms (50.54%) | Memory: 18.6 MB (12.00%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.py
+++ 328-odd-even-linked-list/odd-even-linked-list.py
@@ -5,29 +5,51 @@
 #         self.next = next
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        ##BRUTE FORCE
-        if not head or not head.next:
+        #Opitmal approach
+        if not head or not head.next :
             return head
-        lst=[]
         odd=head
+        evenhead=head.next
         even=head.next
-        while(odd and odd.next):
-            lst.append(odd.val)
-            odd=odd.next.next
-        if(odd):
-            lst.append(odd.val)
         while(even and even.next):
-            lst.append(even.val)
-            even=even.next.next
-        if (even):
-            lst.append(even.val)
-        curr=head
-        i=0
-        while(curr):
-            curr.val=lst[i]
-            i+=1
-            curr=curr.next
+            odd.next=odd.next.next
+            even.next=even.next.next
+            odd=odd.next
+            even=even.next
+        if odd:
+            odd.next
+            
+        odd.next=evenhead
         return head
+            
+            
+        
+        
+        
+        
+        # ##BRUTE FORCE
+        # if not head or not head.next:  #->Base case
+        #     return head
+        # lst=[]
+        # odd=head
+        # even=head.next
+        # while(odd and odd.next):
+        #     lst.append(odd.val)
+        #     odd=odd.next.next
+        # if(odd):
+        #     lst.append(odd.val)
+        # while(even and even.next):
+        #     lst.append(even.val)
+        #     even=even.next.next
+        # if (even):
+        #     lst.append(even.val)
+        # curr=head
+        # i=0
+        # while(curr):
+        #     curr.val=lst[i]
+        #     i+=1
+        #     curr=curr.next
+        # return head
             
             
             
