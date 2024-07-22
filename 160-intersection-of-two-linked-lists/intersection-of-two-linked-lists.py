6f4b866c5fec57a0e22ad44802ff8d8649e923cf
Prudhvi narayana BANDARU <93300939+Prudhvinaraya@users.noreply.github.com>
2024-07-22 17:48:54 +0530

    Time: 85 ms (34.48%) | Memory: 27.3 MB (25.69%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
@@ -6,46 +6,64 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        ##Optimal
         currA=headA
         currB=headB
-        c1=0
-        c2=0
         ##edge case
-        if (currA==None or currB==None):
+        if currA==None or currB==None:
             return None
         
-        ##calculate the length of each ll
-        while(currA):
+        while(currA!=currB):
             currA=currA.next
-            c1+=1 #->5
-        while(currB):
             currB=currB.next
-            c2+=1 #->6
-        ##so we got the two ll lengths
-        currA=headA
-        currB=headB
-        if (c1<c2):
-            d=c2-c1
-            while(d):
-                currB=currB.next
-                d-=1
-            while(currB and currA):
-                if currA==currB:
-                    return currA
-                currA=currA.next
-                currB=currB.next
-            return None
-        else:
-            d=c1-c2
-            while(d):
-                currA=currA.next
-                d-=1
-            while(currA and currB):
-                if currA==currB:
-                    return currA
-                currA=currA.next
-                currB=currB.next
-            return None
+            if currA==currB:
+                return currA
+            if currA==None:
+                currA=headB
+            if currB==None:
+                currB=headA
+        return currA
+        ##->BruteForce
+#         currA=headA
+#         currB=headB
+#         c1=0
+#         c2=0
+#         ##edge case
+#         if (currA==None or currB==None):
+#             return None
+        
+#         ##calculate the length of each ll
+#         while(currA):
+#             currA=currA.next
+#             c1+=1 #->5
+#         while(currB):
+#             currB=currB.next
+#             c2+=1 #->6
+#         ##so we got the two ll lengths
+#         currA=headA
+#         currB=headB
+#         if (c1<c2):
+#             d=c2-c1
+#             while(d):
+#                 currB=currB.next
+#                 d-=1
+#             while(currB and currA):
+#                 if currA==currB:
+#                     return currA
+#                 currA=currA.next
+#                 currB=currB.next
+#             return None
+#         else:
+#             d=c1-c2
+#             while(d):
+#                 currA=currA.next
+#                 d-=1
+#             while(currA and currB):
+#                 if currA==currB:
+#                     return currA
+#                 currA=currA.next
+#                 currB=currB.next
+#             return None
             
             
                 
