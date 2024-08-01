25975b919c5cfd4b8571b965f0731d892a5d3abe
Prudhvi narayana BANDARU <93300939+Prudhvinaraya@users.noreply.github.com>
2024-08-01 15:29:07 +0530

    Time: 31 ms (83.10%) | Memory: 16.4 MB (62.11%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.py
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.py
@@ -2,15 +2,26 @@ class Solution:
     def minBitFlips(self, start: int, goal: int) -> int:
         #find the xor of start ,goal
         #find the no of set bits
-        #->1
-        ans=start^goal
-        ans=bin(ans)
-        ans=ans[2:]
+        #-2
+        xor_=start^goal
         count=0
-        for i in ans:
-            if i=='1':
-                count+=1
+        while xor_:
+            count+=xor_ &1
+            xor_=xor_>>1
         return count
+    
+        
+        #->1
+        # ans1=start^goal
+        # ans=bin(ans1)
+        # ans=ans[2:]
+        # count=0
+        ##counting the number of flip bits
+        # for i in ans:
+        #     if i=='1':
+        #         count+=1
+        
+        #return count
             
         
         
