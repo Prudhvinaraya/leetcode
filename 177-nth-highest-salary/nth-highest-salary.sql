b8c667175352cabc0b6b64fd52055914d94b9e88
Prudhvi narayana BANDARU <93300939+Prudhvinaraya@users.noreply.github.com>
2024-09-23 23:39:19 +0530

    Time: 359 ms (95.01%) | Memory: 0B (100.00%) - LeetSync

--- 177-nth-highest-salary/nth-highest-salary.sql
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -1,11 +1,9 @@
-CREATE FUNCTION getNthHighestSalary(N IN NUMBER) RETURN NUMBER IS
-result NUMBER;
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-SELECT  DISTINCT salary  INTO result FROM 
-(SELECT salary,DENSE_RANK() OVER (ORDER BY salary DESC) R 
-FROM  Employee) WHERE R = N;
-RETURN result;
-EXCEPTION
-WHEN NO_DATA_FOUND THEN
-RETURN NULL;
-END;
+SET N = N-1;
+  RETURN (
+      SELECT DISTINCT(salary) from Employee order by salary DESC
+      LIMIT 1 OFFSET N
+      
+  );
+END
