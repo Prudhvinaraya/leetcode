7bde9ea8569bcf07bc393a0fd39ef0c9508402b2
Prudhvi narayana BANDARU <93300939+Prudhvinaraya@users.noreply.github.com>
2024-09-30 22:18:25 +0530

    Time: 117 ms (91.55%) | Memory: 18.2 MB (25.25%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -2,23 +2,14 @@ class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
-        
         """
-        #two pointer concept
-        intial=0
-        if(len(nums))<2:
-            return nums
-
-        #move the non-zerod to front 
-        for j in range(len(nums)):
-            if nums[j]!=0:
-                nums[intial]=nums[j]
-                intial+=1
-        for f in range(intial,len(nums)):
-            nums[f]=0
-        
-            
+        counter=0
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                nums[counter]=nums[i]
+                counter+=1
+        for i in range(counter,len(nums)):
+            nums[i]=0
         return nums
-        
-        
+                
         
